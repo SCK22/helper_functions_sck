b106086829d0760c79cd6a0acbcac1de6138d7bf
sck22 <chaithanyakumar.ds@gmail.com>
2019-03-23 21:16:34 +0530

    incremental update

--- HelperFunctionsPlotlyPlotting.py
+++ HelperFunctionsPlotlyPlotting.py
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 """Helper Functions to do Plotting"""
-
 
 def generateLayoutBar(col_name):
     """
@@ -71,14 +69,13 @@ def plotBar(dataframe_name, col_name, top_n=None):
             textposition='auto',  # specify at which position on the bar the text should appear
             marker=dict(color='#0047AB'),)]  # change color of the bar
     # color used here Cobalt Blue
-
     layout_bar = generateLayoutBar(col_name=col_name)
-
     fig = go.Figure(data=data, layout=layout_bar)
     return iplot(fig)
 
 
 def plotHist(dataframe, col_name):
+    """Plot histogram"""
     data = [go.Histogram(x=dataframe[col_name],
                          marker=dict(
         color='#CC0E1D',  # Lava (#CC0E1D)
@@ -90,6 +87,7 @@ def plotHist(dataframe, col_name):
 
 
 def plotMultiBox(dataframe, col_name, num_col_name):
+    """Plot multiple box plots based on the levels in a column"""
     data = []
     for i in dataframe[col_name].unique():
         trace = go.Box(y=dataframe[num_col_name][dataframe[col_name] == i],
@@ -98,80 +96,3 @@ def plotMultiBox(dataframe, col_name, num_col_name):
     layout = go.Layout(title="Boxplot of levels in {} for {} column".format(col_name, num_col_name))
     fig = go.Figure(data=data, layout=layout)
     return (iplot(fig))
-=======
-"""Helper Functions to do Plotting"""
-
-
-def generateLayoutBar(col_name):
-    """
-    Generate a layout object for bar chart
-    """
-    layout_bar = go.Layout(
-        autosize=False,  # auto size the graph? use False if you are specifying the height and width
-        width=800,  # height of the figure in pixels
-        height=600,  # height of the figure in pixels
-        title="Distribution of {} column".format(col_name),  # title of the figure
-        # more granular control on the title font
-        titlefont=dict(
-            family='Courier New, monospace',  # font family
-            size=14,  # size of the font
-            color='black'  # color of the font
-        ),
-        # granular control on the axes objects
-        xaxis=dict(
-            tickfont=dict(
-                family='Courier New, monospace',  # font family
-                size=14,  # size of ticks displayed on the x axis
-                color='black'  # color of the font
-            )
-        ),
-        yaxis=dict(
-            #         range=[0,100],
-            title='Percentage',
-            titlefont=dict(
-                size=14,
-                color='black'
-            ),
-            tickfont=dict(
-                family='Courier New, monospace',  # font family
-                size=14,  # size of ticks displayed on the y axis
-                color='black'  # color of the font
-            )
-        ),
-        font=dict(
-            family='Courier New, monospace',  # font family
-            color="white",  # color of the font
-            size=12  # size of the font displayed on the bar
-        )
-    )
-    return layout_bar
-
-
-def plotBar(dataframe_name, col_name):
-    """
-    Plot a bar chart for the categorical columns
-
-    Arguments:
-    dataframe name
-    categorical column name
-
-    Output:
-    Plot
-    """
-    # create a table with value counts
-    temp = dataframe_name[col_name].value_counts()
-    # creating a Bar chart object of plotly
-    data = [go.Bar(
-            x=temp.index.astype(str),  # x axis values
-            y=np.round(temp.values.astype(float) / temp.values.sum(), 4) * 100,  # y axis values
-            text=['{}%'.format(i) for i in np.round(temp.values.astype(float) / temp.values.sum(), 4) * 100],
-            # text to be displayed on the bar, we are doing this to display the '%' symbol along with the number on the bar
-            textposition='auto',  # specify at which position on the bar the text should appear
-            marker=dict(color='#0047AB'),)]  # change color of the bar
-    # color used here Cobalt Blue
-
-    layout_bar = generateLayoutBar(col_name=col_name)
-
-    fig = go.Figure(data=data, layout=layout_bar)
-    return iplot(fig)
->>>>>>> 77c24bfa326973f9cdad90f5bad755b01a86310c
